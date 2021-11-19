ad0b8a279588217d4d1ebfc3e9f671cea1126c33
Lieba Pil <pil.lieba@gmail.com>
2021-11-18 17:12:10 -0800

    wrote the begining syntax

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const column = document.querySelector('.column1')
+const column = document.querySelectorAll('.circle')
+let currentPlayer= 'red'
+// column.addEventListener('click',()=>{
+//     column.children[5].classList.add('red')
+//     column.children[4].classList.add('black')
+    
+// })
 
-column.addEventListener('click',()=>{
-    column.children[5].classList.add('red')
-})
+for (let i = 0; i < column.length; i++){
+    
+    column[i].addEventListener('click',()=>{
+        if (currentPlayer === 'red'){
+        column[i].classList.add('red')
+        currentPlayer='black'
+        }else {
+            column[i].classList.add('black')
+            currentPlayer = 'red'   
+        }})
+
+}
