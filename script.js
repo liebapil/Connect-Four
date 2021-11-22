a9f1e3dd8511b4e8f0c4b29bf562aa95c1d50751
Lieba Pil <pil.lieba@gmail.com>
2021-11-22 12:24:38 -0800

    cleaned up the code in java

--- script.js
+++ script.js
@@ -105,100 +105,6 @@ const winningArray = [
 
 
 
-
-    // const howToWin = () =>{
-    //     if  (column[0].currentTarget === currentPlayer && column[1].currentTarget === currentPlayer && column[2].currentTarget === currentPlayer && column[3].currentTarget === currentPlayer){
-    //     alert(currentPlayer + ' wins')
-        //isGameOver=true
-       // }
-        // else if  (column[1] === currentPlayer && column[2] === currentPlayer && column[3] === currentPlayer && column[4] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[2] === currentPlayer && column[3] === currentPlayer && column[4] === currentPlayer && column[5] === currentPlayer){
-        //         alert(currentPlayer + ' wins')
-        //         isGameOver=true
-        // }
-        // else if  (column[6] === currentPlayer && column[7] === currentPlayer && column[8] === currentPlayer && column[9] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if  (column[7] === currentPlayer && column[8] === currentPlayer && column[9] === currentPlayer && column[10] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if  (column[8] === currentPlayer && column[9] === currentPlayer && column[10] === currentPlayer && column[11] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if  (column[12] === currentPlayer && column[13] === currentPlayer && column[14] === currentPlayer && column[15] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[13] === currentPlayer && column[14] === currentPlayer && column[15] === currentPlayer && column[16] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if  (column[14] === currentPlayer && column[15] === currentPlayer && column[16] === currentPlayer && column[17] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[18] === currentPlayer && column[19] === currentPlayer && column[20] === currentPlayer && column[21] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if  (column[19] === currentPlayer && column[20] === currentPlayer && column[21] === currentPlayer && column[22] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if  (column[20] === currentPlayer && column[21] === currentPlayer && column[22] === currentPlayer && column[23] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if  (column[24] === currentPlayer && column[25] === currentPlayer && column[26] === currentPlayer && column[27] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[25] === currentPlayer && column[26] === currentPlayer && column[27] === currentPlayer && column[28] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[26] === currentPlayer && column[27] === currentPlayer && column[28] === currentPlayer && column[29] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[30] === currentPlayer && column[31] === currentPlayer && column[32] === currentPlayer && column[33] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[31] === currentPlayer && column[32] === currentPlayer && column[33] === currentPlayer && column[34] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[32] === currentPlayer && column[33] === currentPlayer && column[34] === currentPlayer && column[35] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[36] === currentPlayer && column[37] === currentPlayer && column[38] === currentPlayer && column[39] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[37] === currentPlayer && column[38] === currentPlayer && column[39] === currentPlayer && column[40] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-        // else if (column[38] === currentPlayer && column[39] === currentPlayer && column[40] === currentPlayer && column[41] === currentPlayer){
-        //     alert(currentPlayer + ' wins')
-        //     isGameOver=true
-        // }
-
-        
-        
-   // }  
-
-
-
-
 for (let i = 0; i < circle.length; i++){
     circle[i].addEventListener('click',(e)=>{
         console.log(e.currentTarget)
@@ -337,21 +243,8 @@ for (let i = 0; i < circle.length; i++){
         else {
             return "You picked the wrong sqaure"
         }
-
-
-
-
-
-    })
-    //     if (currentPlayer === 'red'){
-    //     circle[i].classList.add('red')
-    //     //howToWin()
-    //     currentPlayer='yellow'
-    //     }else {
-    //         circle[i].classList.add('yellow')
-    //         //howToWin()
-    //         currentPlayer = 'red'   
-    // }})
+    }
+    )
 }
 /////////////
 }
